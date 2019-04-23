1c545ca120789d4601cad619dad8412e047e5ecf
Kevin Garvey and Mariia Stashuk <kevin@>
2019-04-23 16:40:54 -0700

    add tests

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,31 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTest
+  {
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      //Act
+      string result = newItem.GetDescription();
+      // Assert
+      Assert.AreEqual(description,result);
+    }
+
+  }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,13 @@
+namespace ToDoList.Models
+{
+  public class Item // class
+  {
+    private string _description; // field
+
+    public Item (string description) // constructor
+    {
+      _description = description;
+    }
+
+  }
+}
