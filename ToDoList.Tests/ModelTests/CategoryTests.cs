78bfeae11dd4d8e93b4bbd677147c1a9ed5a6c94
Mariia <mariiapopovych@gmail.com>
2019-05-01 18:12:48 -0700

    add category file

---
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -0,0 +1,69 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class CategoryTest :IDisposable
+  {
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
+
+    [TestMethod]
+      public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+      {
+        Category newCategory = new Category("test category");
+        Assert.AreEqual(typeof(Category), newCategory.GetType());
+      }
+
+    [TestMethod]
+     public void GetName_ReturnsName_String()
+     {
+       //Arrange
+       string name = "Test Category";
+       Category newCategory = new Category(name);
+
+       //Act
+       string result = newCategory.GetName();
+
+       //Assert
+       Assert.AreEqual(name, result);
+     }
+
+
+     [TestMethod]
+      public void GetId_ReturnsCategoryId_Int()
+      {
+        //Arrange
+        string name = "Test Category";
+        Category newCategory = new Category(name);
+
+        //Act
+        int result = newCategory.GetId();
+
+        //Assert
+        Assert.AreEqual(3, result);
+      }
+
+      [TestMethod]
+        public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+        {
+          //Arrange
+          string name01 = "Work";
+          string name02 = "School";
+          Category newCategory1 = new Category(name01);
+          Category newCategory2 = new Category(name02);
+          List<Category> newList = new List<Category> { newCategory1, newCategory2 };
+
+          //Act
+          List<Category> result = Category.GetAll();
+
+          //Assert
+          CollectionAssert.AreEqual(newList, result);
+        }
+  }
+}
